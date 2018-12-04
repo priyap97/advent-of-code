45c30d5080f0b6f2032043fde43c0cc45a3eae71
priyap97 <priya.pamnani@rutgers.edu>
2018-12-04 00:34:31 -0500

    day 3 documentation

--- day3/day3.py
+++ day3/day3.py
@@ -1,7 +1,10 @@
-input = open('scratch.txt')
+input = open('input.txt')
 hash = {}
 resultset = set()
+
+# Part 1
 for line in input.readlines():
+    # Parse input
     split1 = line.split(' ')
     coordinates = split1[2][:-1].split(',')
     size = split1[3].split('x')
@@ -10,6 +13,7 @@ for line in input.readlines():
     x_size = int(size[0])
     y_size = int(size[1])
 
+    # For each square inch in a claim, if it already has been used in another claim we add to resultset
     for i in range(x, x + x_size):
         for j in range(y, y + y_size):
             if (i,j) in hash:
@@ -18,9 +22,14 @@ for line in input.readlines():
                 hash[(i, j)] = hash[(i, j)] + 1
             hash[(i,j)] = 1
 
+# Result Part 1
+print('Result Part 1', len(resultset))
 input.close()
-input = open('scratch.txt')
+
+# Result Part 2
+input = open('input.txt')
 for line in input.readlines():
+    # Parse input
     split1 = line.split(' ')
     coordinates = split1[2][:-1].split(',')
     size = split1[3].split('x')
@@ -29,11 +38,15 @@ for line in input.readlines():
     x_size = int(size[0])
     y_size = int(size[1])
 
+    # Search each square inch in a claim
+    # If it is in resultset then we know it cannot be the answer, so we set overlap to True
     overlap = False
     for i in range(x, x + x_size):
         for j in range(y, y + y_size):
             if (i,j) in resultset:
                 overlap = True
 
+    # If overlap is False, print the claim number
     if not(overlap):
-        print(split1[0])
+        print('Result Part 2: ', split1[0])
+        break
